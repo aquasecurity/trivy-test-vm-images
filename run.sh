bc28167459638d4b9fb4fe3aea33537304f3c256
masahiro331 <m_fujimura@r.recruit.co.jp>
2022-10-16 15:38:19 +0900

    feat(script): add create-image handling

--- run.sh
+++ run.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 set -e
 
-export BUCKET_NAME=
-export PREFIX=
-export REPO=
+export BUCKET_NAME=a
+export PREFIX=a
+export REPO=a
+
+if [ -e $REPO ]; then
+  echo "REPO variable is empty"
+  exit 1
+fi
+
+if [ -e $PREFIX ]; then
+  echo "PREFIX variable is empty"
+  exit 1
+fi
+
+if [ -e $BUCKET_NAME ]; then
+  echo "BUCKET_NAME variable is empty"
+  exit 1
+fi
+
+ret=$(aws ec2 describe-images --filters "Name=tag:Description,Values=created by packer" | jq -r ".Images[].ImageId")
+if [ -z "$ret" ]; then
+  ./scripts/create-images.sh
+fi
 
 if [ -e "images/" ]; then
   rm -f images/**
 fi
 
-if [ -e $REPO ]
-then
-  echo "REPO variable is empty"
-  exit 1
-fi
-
-if [ -e $PREFIX ]
-then
-  echo "PREFIX variable is empty"
-  exit 1
-fi
-
-if [ -e $BUCKET_NAME ]
-then
-  echo "BUCKET_NAME variable is empty"
-  exit 1
-fi
-
-# ./scripts/create-images.sh
 ./scripts/create-and-download-vmdk.sh
 ./scripts/download-snapsthots.sh
 ./scripts/push-images.sh
