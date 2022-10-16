4607e03768aa9379df5aa4214847f29b5564f144
masahiro331 <m_fujimura@r.recruit.co.jp>
2022-10-16 15:14:40 +0900

    feat(script): check variable empty

--- run.sh
+++ run.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 set -e
 
-export BUCKET_NAME=export-aws-images-masahiro331
-export PREFIX=images/
-export REPO=ghcr.io/masahiro331/test-vm
+export BUCKET_NAME=
+export PREFIX=
+export REPO=
 
 if [ -e "images/" ]; then
   rm -f images/**
+fi
+
+if [ -e $REPO ]
+then
+  echo "REPO variable is empty"
+  exit 1
+fi
+
+if [ -e $PREFIX ]
+then
+  echo "PREFIX variable is empty"
+  exit 1
+fi
+
+if [ -e $BUCKET_NAME ]
+then
+  echo "BUCKET_NAME variable is empty"
+  exit 1
 fi
 
 # ./scripts/create-images.sh
